f2c48d06ba64bee1867dec57a8938cf1a05ba743
Ahmad Alkharfan <41472767+Ahmadalkharfan@users.noreply.github.com>
2022-04-02 15:19:34 +0800

    config react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; //important to overwrite
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
